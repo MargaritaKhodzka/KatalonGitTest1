0b49ae298ae7a372bafef022a14c138a12cdc1aa
khodzkm <khodzkm@NYCDD29BGB2.Willkie.com>
2018-07-18 12:52:01 -0400

    Test 1 A

---
+++ Object Repository/Test A_OR/Page_OrangeHRM/img.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img</name>
+   <tag></tag>
+   <elementGuidId>9cc0b098-e884-47a5-8f21-076772953fe7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>src</name>
+      <type>Main</type>
+      <value>/webres_5acde3dbd3adc6.90334155/orangehrmTimePlugin/images/MyTimesheet.png</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;dashboard-quick-launch-panel-menu_holder&quot;)/table[@class=&quot;quickLaungeContainer&quot;]/tbody[1]/tr[1]/td[3]/div[@class=&quot;quickLaunge&quot;]/a[1]/img[1]</value>
+   </webElementProperties>
+</WebElementEntity>
